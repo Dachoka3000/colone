4bb48bd11200ca937839ebf819e3cc52c9e90e42
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-27 00:46:08 +0300

    add a project model

--- work/models.py
+++ work/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length = 30)
+    image = models.ImageField(upload_t0 = 'photos/', blank = True)
+    description = models.TextField()
+    link = models.URLField(max_length = 200)
+    owner = models.ForeignKey(User,on_delete = models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add = True, blank =True)
