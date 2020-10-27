621380ca0ccc5420588dcbb5154bcf0187ca6c63
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-27 05:00:31 +0300

    add a model for user profile

--- work/models.py
+++ work/models.py
@@ -5,8 +5,15 @@ from django.contrib.auth.models import User
 
 class Project(models.Model):
     title = models.CharField(max_length = 30)
-    image = models.ImageField(upload_t0 = 'photos/', blank = True)
+    image = models.ImageField(upload_to = 'photos/', blank = True)
     description = models.TextField()
     link = models.URLField(max_length = 200)
     owner = models.ForeignKey(User,on_delete = models.CASCADE)
     date_added = models.DateTimeField(auto_now_add = True, blank =True)
+
+
+class Profile(models.Model):
+    pic = models.ImageField(upload_to='images/', blank = True)
+    bio = models.TextField(blank=True)
+    contact = models.TextField()
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
