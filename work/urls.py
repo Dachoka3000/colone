df597bc9d29da385f6853809937b7efc802dc1da
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-28 17:08:23 +0300

    add url paths for project and project detail

--- work/urls.py
+++ work/urls.py
@@ -8,6 +8,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
+    path('project/',views.project, name = 'project'),
+    path('projectdetail/<int:project_id>/', views.projectdetail, name = 'projectdetail'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
