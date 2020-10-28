858f63aff5fe34f1bb792ed3cab0b1ae58129583
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-29 00:57:50 +0300

    add a form to add a user profile

--- work/forms.py
+++ work/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from .models import Project,Profile
 
-class UploadProject(forms.Form):
+class UploadProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ['owner','date_added']
+
+class AddProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
