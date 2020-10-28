93085a6169735032876338281d1021831b75f7f7
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-28 16:47:29 +0300

    add a view function to display all the projects

--- work/views.py
+++ work/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Profile,Project
 
 # Create your views here.
+def home(request):
+
+    return render(request,'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def project(request):
+    projects = Project.objects.all()
+    return render(request, 'project.html',{"projects":projects})
+
+def projectdetail(request, project_id):
+    try:
+        project = Project.objects.get(id=project_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,'projectdetail.html', {"project": project})
