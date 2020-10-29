6ea4e8b5577b0a566d33a404cb3e7482ee059757
Machoka Daisy <machokadaisy25@gmail.com>
2020-10-29 08:24:34 +0300

    add a form for rating projects

--- work/forms.py
+++ work/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Project,Profile
+from .models import Project,Profile,Rating
 
 class UploadProjectForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,8 @@ class AddProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+class AddRatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        exclude = ['project','human']
